1852d91ab02f43ef9f08e6f63652d5a201c7337f
CyrusLeGrand <cyrus.achemenide@gmail.com>
2023-05-30 16:09:49 +0200

    dropdown updated

--- scripts/utils/dropdowns.js
+++ scripts/utils/dropdowns.js
@@ -6,12 +6,53 @@ export function createDropdownSection() {
     const block = document.createElement('div');
     block.className = 'dropdown__block dropdown__block--' + type;
 
+    const inputWrapper = document.createElement('div');
+    inputWrapper.className = 'dropdown__input-wrapper';
+
     const input = document.createElement('input');
     input.type = 'text';
     input.className = 'dropdown__input dropdown__input--' + type;
     input.placeholder = type;
 
-    block.appendChild(input);
+    const chevron = document.createElement('i');
+    chevron.className = 'fa-solid fa-chevron-down';
+
+    inputWrapper.appendChild(input);
+    inputWrapper.appendChild(chevron);
+    block.appendChild(inputWrapper);
+
+    const dropdownList = document.createElement('ul');
+    dropdownList.className = 'dropdown__list dropdown__list--' + type;
+
+    block.appendChild(dropdownList);
+
+    if (type === 'Ingredients' || 'Appareils' || 'Ustensiles') {
+      const ingredientInput = document.createElement('input');
+      ingredientInput.type = 'text';
+      ingredientInput.className = 'dropdown__ingredient-input';
+      ingredientInput.placeholder = 'Chercher une recette';
+      ingredientInput.style.display = 'none';
+
+      block.appendChild(ingredientInput);
+
+      let isIngredientInputVisible = false;
+
+      input.addEventListener('click', () => {
+        if (isIngredientInputVisible) {
+          ingredientInput.style.display = 'none';
+          dropdownList.classList.remove('show');
+        } else {
+          ingredientInput.style.display = 'block';
+          dropdownList.classList.add('show');
+        }
+
+        isIngredientInputVisible = !isIngredientInputVisible;
+      });
+    } else {
+      input.addEventListener('click', () => {
+        dropdownList.classList.toggle('show');
+      });
+    }
 
     return block;
   }
