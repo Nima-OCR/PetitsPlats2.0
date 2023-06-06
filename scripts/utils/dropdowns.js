f7978a6385c22384476fe2ae267aca8347d7c54a
CyrusLeGrand <cyrus.achemenide@gmail.com>
2023-06-06 17:56:23 +0200

    dropdown.js & scss updated

--- scripts/utils/dropdowns.js
+++ scripts/utils/dropdowns.js
@@ -2,16 +2,17 @@ export function createDropdownSection() {
   const dropdownSection = document.getElementById('dropdown');
   dropdownSection.id = 'dropdown';
 
-  function createDropdownBlock(type) {
+  function createBlock(type) {
+
     const block = document.createElement('div');
-    block.className = 'dropdown__block dropdown__block--' + type;
+    block.className = `dropdown__block dropdown__block--${type}`;
 
     const inputWrapper = document.createElement('div');
     inputWrapper.className = 'dropdown__input-wrapper';
 
     const input = document.createElement('input');
     input.type = 'text';
-    input.className = 'dropdown__input dropdown__input--' + type;
+    input.className = `dropdown__input dropdown__input--${type}`;
     input.placeholder = type;
 
     const chevron = document.createElement('i');
@@ -22,64 +23,60 @@ export function createDropdownSection() {
     block.appendChild(inputWrapper);
 
     const dropdownList = document.createElement('ul');
-    dropdownList.className = 'dropdown__list dropdown__list--' + type;
+    dropdownList.className = `dropdown__list dropdown__list--${type}`;
 
     block.appendChild(dropdownList);
 
-    if (type === 'Ingredients') {
-      const ingredientInputDivElement = document.createElement('div');
-      ingredientInputDivElement.className = 'dropdown__ingredient-div';
-      block.appendChild(ingredientInputDivElement);
+    const inputDivElement = document.createElement('div');
+    inputDivElement.className = `dropdown__${type.toLowerCase()}-div`;
+    block.appendChild(inputDivElement);
 
-      const divSearchInputElement = document.createElement('div');
-      divSearchInputElement.className = 'dropdown__ingredient-search';
-      ingredientInputDivElement.appendChild(divSearchInputElement);
+    const divSearchInputElement = document.createElement('div');
+    divSearchInputElement.className = `dropdown__${type.toLowerCase()}-search`;
+    inputDivElement.appendChild(divSearchInputElement);
 
+    const customInput = document.createElement('input');
+    customInput.type = 'text';
+    customInput.className = `dropdown__${type.toLowerCase()}-input`;
+    customInput.placeholder = `Chercher un ${type.toLowerCase()}`;
+    customInput.style.display = 'none';
+    divSearchInputElement.appendChild(customInput);
 
-      const ingredientInput = document.createElement('input');
-      ingredientInput.type = 'text';
-      ingredientInput.className = 'dropdown__ingredient-input';
-      ingredientInput.placeholder = 'Chercher une recette';
-      ingredientInput.style.display = 'none';
-      divSearchInputElement.appendChild(ingredientInput);
+    const icon = document.createElement('i');
+    icon.className = `fas fa-search dropdown__${type.toLowerCase()}-search__icon`;
+    icon.style.display = 'none';
+    divSearchInputElement.appendChild(icon);
 
-      const icon = document.createElement('i');
-      icon.className = 'fas fa-search dropdown__ingredient-search__icon';
-      icon.style.display = 'none';
-      divSearchInputElement.appendChild(icon);
+    let isInputVisible = false;
 
-      let isIngredientInputVisible = false;
-
-      input.addEventListener('click', () => {
-        if (isIngredientInputVisible) {
-          icon.style.display = 'none'; // cache la loupe lorsque le menu est fermé
-          ingredientInputDivElement.style.display = 'none';
-          ingredientInput.style.display = 'none';
-          dropdownList.classList.remove('show');
-          chevron.classList.remove('up');
-        } else {
-          icon.style.display = 'block'; // affiche la loupe lorsque le menu est déroulé
-          ingredientInputDivElement.style.display = 'block';
-          ingredientInput.style.display = 'block';
-          divSearchInputElement.style.margin = "15px 0px 15px 0px";
-          divSearchInputElement.style.border = "1px solid lightgrey";
-          dropdownList.classList.add('show');
-          chevron.classList.add('up');
-        }
-
-        isIngredientInputVisible = !isIngredientInputVisible;
-      });
-    }
+    input.addEventListener('click', () => {
+      if (isInputVisible) {
+        icon.style.display = 'none';
+        inputDivElement.style.display = 'none';
+        customInput.style.display = 'none';
+        dropdownList.classList.remove('show');
+        chevron.classList.remove('up');
+      } else {
+        icon.style.display = 'block';
+        inputDivElement.style.display = 'block';
+        customInput.style.display = 'block';
+        divSearchInputElement.style.border = '1px solid lightgrey';
+        dropdownList.classList.add('show');
+        chevron.classList.add('up');
+      }
+      isInputVisible = !isInputVisible;
+    });
 
     return block;
   }
 
-  const ingredientBlock = createDropdownBlock('Ingredients');
+  const ingredientsBlock = createBlock('Ingredients');
+  const appareilsBlock = createBlock('Appareils');
+  const ustensilesBlock = createBlock('Ustensiles');
 
-
-  dropdownSection.appendChild(ingredientBlock);
-
-
+  dropdownSection.appendChild(ingredientsBlock);
+  dropdownSection.appendChild(appareilsBlock);
+  dropdownSection.appendChild(ustensilesBlock);
 
   const mainElement = document.querySelector('main');
   mainElement.appendChild(dropdownSection);
