92a8f258b4ad39f974495b5a03568ae45d37b373
CyrusLeGrand <cyrus.achemenide@gmail.com>
2023-06-08 17:06:23 +0200

    searchNativeLoops.js updated

--- scripts/utils/searchNativeLoops.js
+++ scripts/utils/searchNativeLoops.js
@@ -1,4 +1,4 @@
-import {recipesOld} from "../../data/recipesOld.js";
+import { recipesOld } from "../../data/recipesOld.js";
 
 // Cette fonction vérifie si une chaîne de caractères (item) contient une autre chaîne (query)
 function doesMatch(item, query) {
@@ -16,57 +16,61 @@ function ingredientsMatchQuery(ingredients, query) {
   return false;
 }
 
-// Cette fonction crée un élément de type 'div' pour une recette, contenant le nom de la recette
-function createRecipeElement(recipe) {
-  const recipeElement = document.createElement("div");
-  recipeElement.textContent = recipe.name;
-  return recipeElement;
-}
-
-
-// Cette fonction met à jour l'interface utilisateur avec les résultats de la recherche. Si aucun résultat n'est trouvé, un message est affiché à l'utilisateur.
+// Cette fonction met à jour l'interface utilisateur avec les résultats de la recherche. Si aucun résultat n'est trouvé, affiche toutes les recettes.
 function updateSearchResults(results) {
   hideAllRecipes();
 
-  for (let i = 0; i < results.length; i++) {
-    const recipeElement = document.getElementById(results[i].id);
-    if (recipeElement) {
-      recipeElement.style.display = 'block';
+  if (results.length === 0) {
+    // Afficher toutes les recettes si aucun résultat n'est trouvé
+    for (let i = 0; i < recipesOld.length; i++) {
+      const recipeElement = document.getElementById(recipesOld[i].id);
+      if (recipeElement) {
+        recipeElement.style.display = 'block';
+      }
+    }
+  } else {
+    // Afficher les recettes correspondantes aux résultats de recherche
+    for (let i = 0; i < results.length; i++) {
+      const recipeElement = document.getElementById(results[i].id);
+      if (recipeElement) {
+        recipeElement.style.display = 'block';
+      }
     }
   }
 }
 
-
-
-// Cette fonction cherche les recettes qui correspondent à la recherche dans le nom, les ingrédients et la description et contient au moins 3 caractères.
+// Cette fonction effectue la recherche de recettes en fonction de la chaîne de recherche fournie
 export function searchRecipes(query) {
   const lowerCaseQuery = query.toLowerCase();
   const searchResults = [];
 
   if (lowerCaseQuery.length === 0) {
+    // Afficher toutes les recettes si la chaîne de recherche est vide
     updateSearchResults(recipesOld);
     return;
   }
 
-  if (lowerCaseQuery.length >= 3) {
-    for (let i = 0; i < recipesOld.length; i++) {
-      const recipe = recipesOld[i];
+  if (lowerCaseQuery.length < 3) {
+    return; // La recherche n'est pas effectuée si la chaîne de recherche contient moins de 3 caractères
+  }
 
-      if (
-        doesMatch(recipe.name, lowerCaseQuery) ||
-        ingredientsMatchQuery(recipe.ingredients, lowerCaseQuery) ||
-        doesMatch(recipe.description, lowerCaseQuery)
-      ) {
-        searchResults.push(recipe);
-      }
+  for (let i = 0; i < recipesOld.length; i++) {
+    const recipe = recipesOld[i];
+
+    if (
+      doesMatch(recipe.name, lowerCaseQuery) ||
+      ingredientsMatchQuery(recipe.ingredients, lowerCaseQuery) ||
+      doesMatch(recipe.description, lowerCaseQuery)
+    ) {
+      searchResults.push(recipe);
     }
   }
 
   updateSearchResults(searchResults);
 }
 
-
-function hideAllRecipes() {
+// Cette fonction masque toutes les recettes de l'interface utilisateur
+export function hideAllRecipes() {
   const recipes = document.getElementsByClassName('recipe-article');
   for (let i = 0; i < recipes.length; i++) {
     recipes[i].style.display = 'none';
