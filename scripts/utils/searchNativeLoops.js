487d35c740355807f69c0c341cdbb441d2aa591d
CyrusLeGrand <cyrus.achemenide@gmail.com>
2023-06-08 18:42:29 +0200

    searchNativeLoops.js updated

--- scripts/utils/searchNativeLoops.js
+++ scripts/utils/searchNativeLoops.js
@@ -1,27 +1,44 @@
 import { recipesOld } from "../../data/recipesOld.js";
 
-// Cette fonction vérifie si une chaîne de caractères (item) contient une autre chaîne (query)
-function doesMatch(item, query) {
+// Permet de vérifier si une partie du texte correspond à la recherche effectuée par l'utilisateur.
+function textIncludes(item, query) {
   const lowerCaseItem = item.toLowerCase();
   return lowerCaseItem.includes(query);
 }
 
-// Cette fonction vérifie si un des ingrédients d'une recette contient la chaîne de recherche (query)
-function ingredientsMatchQuery(ingredients, query) {
+// Vérification de correspondance d'une recette avec une requête
+function isQueryContainedInRecipe(name, ingredients, description, query) {
+  if (textIncludes(name, query)) {
+    return true;
+  }
+
   for (let i = 0; i < ingredients.length; i++) {
-    if (doesMatch(ingredients[i].ingredient, query)) {
+    if (textIncludes(ingredients[i].ingredient, query)) {
       return true;
     }
   }
+
+  if (textIncludes(description, query)) {
+    return true;
+  }
+
   return false;
 }
 
-// Cette fonction met à jour l'interface utilisateur avec les résultats de la recherche. Si aucun résultat n'est trouvé, affiche toutes les recettes.
+// Cette fonction masque toutes les recettes de l'interface utilisateur
+function hideAllRecipes() {
+  const recipes = document.getElementsByClassName('recipe-article');
+  for (let i = 0; i < recipes.length; i++) {
+    recipes[i].style.display = 'none';
+  }
+}
+
+// Met à jour l'interface utilisateur avec les résultats de la recherche.
 function updateSearchResults(results) {
   hideAllRecipes();
 
-  if (results.length === 0) {
-    // Afficher toutes les recettes si aucun résultat n'est trouvé
+  if (!results.length) {
+    // Affiche toutes les recettes si aucun résultat n'est trouvé
     for (let i = 0; i < recipesOld.length; i++) {
       const recipeElement = document.getElementById(recipesOld[i].id);
       if (recipeElement) {
@@ -29,7 +46,7 @@ function updateSearchResults(results) {
       }
     }
   } else {
-    // Afficher les recettes correspondantes aux résultats de recherche
+    // Affiche les recettes correspondantes aux résultats de recherche
     for (let i = 0; i < results.length; i++) {
       const recipeElement = document.getElementById(results[i].id);
       if (recipeElement) {
@@ -39,28 +56,27 @@ function updateSearchResults(results) {
   }
 }
 
-// Cette fonction effectue la recherche de recettes en fonction de la chaîne de recherche fournie
+// Effectue la recherche de recettes en fonction de l'input fournie
 export function searchRecipes(query) {
   const lowerCaseQuery = query.toLowerCase();
   const searchResults = [];
 
   if (lowerCaseQuery.length === 0) {
-    // Afficher toutes les recettes si la chaîne de recherche est vide
+    // Affiche toutes les recettes si la recherche est vide
     updateSearchResults(recipesOld);
     return;
   }
 
   if (lowerCaseQuery.length < 3) {
-    return; // La recherche n'est pas effectuée si la chaîne de recherche contient moins de 3 caractères
+    return; // La recherche n'est pas effectuée si elle contient moins de 3 caractères
   }
 
   for (let i = 0; i < recipesOld.length; i++) {
     const recipe = recipesOld[i];
 
     if (
-      doesMatch(recipe.name, lowerCaseQuery) ||
-      ingredientsMatchQuery(recipe.ingredients, lowerCaseQuery) ||
-      doesMatch(recipe.description, lowerCaseQuery)
+      textIncludes(recipe.name, lowerCaseQuery) ||
+      isQueryContainedInRecipe(recipe.name, recipe.ingredients, recipe.description, lowerCaseQuery)
     ) {
       searchResults.push(recipe);
     }
@@ -69,10 +85,4 @@ export function searchRecipes(query) {
   updateSearchResults(searchResults);
 }
 
-// Cette fonction masque toutes les recettes de l'interface utilisateur
-export function hideAllRecipes() {
-  const recipes = document.getElementsByClassName('recipe-article');
-  for (let i = 0; i < recipes.length; i++) {
-    recipes[i].style.display = 'none';
-  }
-}
+
